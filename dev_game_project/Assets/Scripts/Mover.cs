e63f7e9626e8f16d98a9fa3039801d3408bb0abc
Aneri20 <anerishah.2059@gmail.com>
2016-04-16 23:15:27 -0700

    #33-adding shots

---
+++ dev_game_project/Assets/Scripts/Mover.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+    public GameObject object;
+    public float speed;
+    void start()
+    {
+        RigidBody rb = objectGetComponent<RigidBody>
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+}
