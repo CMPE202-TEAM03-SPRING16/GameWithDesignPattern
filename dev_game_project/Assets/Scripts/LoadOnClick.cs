3bc7b0bd2c4286863249726f52ffaae3ae071947
Aneri20 <anerishah.2059@gmail.com>
2016-04-23 16:43:37 -0700

    load on click script added

---
+++ dev_game_project/Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class LoadOnClick : MonoBehaviour,Receiver {
+
+	public GameObject loadingImage;
+
+	void OnStart(){
+	}
+
+	public void LoadScene(){
+		Debug.Log ("Load scene");
+		//Application.LoadLevel(1);
+		SceneManager.LoadScene (1);
+
+	}
+
+	public void QuitGame(){
+		Debug.Log ("in quit game");
+		SceneManager.LoadScene (0);
+	}
+}
+
