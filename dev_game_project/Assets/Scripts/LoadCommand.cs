a76e84413274de83c98c03b2c961303bead091e0
Ekta Sorathia <ektasorathia@gmail.com>
2016-04-23 16:37:01 -0700

    Command pattern added

---
+++ dev_game_project/Assets/Scripts/LoadCommand.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadCommand : Command{
+
+	private Receiver theReceiver;
+
+	public LoadCommand (Receiver receiver){
+		theReceiver = receiver;
+	}
+
+	void Command.execute(){
+		Debug.Log ("in con Load scene");
+		theReceiver.LoadScene();
+	}
+}
---
+++ dev_game_project/Assets/Scripts/QuitMenu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+public class QuitMenu: MonoBehaviour {
+	Receiver receiver ;
+	Command c;
+	Command s;
+	ConcreteInvoker i;
+	public bool isQuit;
+
+	public void Awake(){
+		receiver = gameObject.AddComponent<LoadOnClick>();
+		c = new QuitCommand(receiver);
+		s = new LoadCommand(receiver);
+		i= new ConcreteInvoker(s,c);
+	}
+
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Escape)) 
+		{ 
+			Application.LoadLevel(0);
+		}
+	}
+
+	public void OnClick(){
+		Debug.Log("on mouse" + isQuit);
+		i.gameQuit();
+	}
+}
+
+
+
