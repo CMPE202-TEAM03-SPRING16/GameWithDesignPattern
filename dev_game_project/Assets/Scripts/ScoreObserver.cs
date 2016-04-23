83bd22bbcb6f98328635731ef5ada954269aac9b
Gaurav Chodwadia <gauravchodwadia@gmail.com>
2016-04-23 16:28:18 -0700

    ScoreObserver Script created.

---
+++ dev_game_project/Assets/Scripts/ScoreObserver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class ScoreObserver :MonoBehaviour, IScoreObserver{
+	Game_Controller player = new Game_Controller();
+	private static ScoreObserver observer;
+	private int playerPoints;
+
+	public ScoreObserver(){
+		playerPoints = 0;
+	}
+
+	public void updatePoints(int points){
+		playerPoints = playerPoints + points;
+	}
+
+	public int getPlayerPoints(){
+		return playerPoints;
+	}
+
+	public static ScoreObserver getInstance(){
+		if (observer == null) {
+			observer = new ScoreObserver ();
+		}
+		return observer;
+	}
+}
