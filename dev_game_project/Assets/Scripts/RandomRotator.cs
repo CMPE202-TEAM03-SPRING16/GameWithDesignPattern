a0f2a59241cf27926b8a2acc8828d8e31a5130bf
Darshan Modh <darshanmodh@gmail.com>
2016-04-23 20:54:00 -0800

    Modified RandomRotator

--- dev_game_project/Assets/Scripts/RandomRotator.cs
+++ dev_game_project/Assets/Scripts/RandomRotator.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public class RandomRotator : MonoBehaviour 
 {
 	public float tumble;
-
 	void Start ()
 	{
 		GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * tumble;
 	}
 }
+
