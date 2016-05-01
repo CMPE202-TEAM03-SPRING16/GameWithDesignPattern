406684dbf74dba861ccfeae8b8dac79706f41aeb
Ekta Sorathia <ektasorathia@gmail.com>
2016-04-30 19:58:14 -0700

    Factory Pattern Implemented

---
+++ dev_game_project/Assets/Scripts/EnemyFactory.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+
+
+public class EnemyFactory : MonoBehaviour
+{
+	public void getEnemyShip(GameObject ship, Vector3 spawnPosition, Quaternion spawnRotation){
+
+		String shipName = ship.name;
+		Debug.Log ("Cuurent Ship"+shipName);
+		if (shipName == "EnemyShip") {
+			new WeaponController ().enemyShip (ship, spawnPosition, spawnRotation);
+		} 
+		if (shipName == "health") {
+			Instantiate (ship, spawnPosition, spawnRotation);
+		}
+		if (shipName == "EnemyShip1") {
+			new WeaponController().enemyShip (ship, spawnPosition, spawnRotation);
+		} 
+		if (shipName == "Power") {
+			new PowerController().enemyShip (ship, spawnPosition, spawnRotation);
+		}
+
+	}
+}
+
+
