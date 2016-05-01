a14fb9ef5926e2759d13c7b986223400e6f0f332
chirag-orbittec <chirag.orbittec@gmail.com>
2016-04-30 22:57:17 -0700

    Strategy implemented

---
+++ dev_game_project/Assets/Scripts/MediumSpeedStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+	public class MediumSpeedStrategy : ISpeedStrategy
+	{
+		public MediumSpeedStrategy ()
+		{
+
+		}
+
+	public void SetSpeed(Mover character,int speed){
+			character.speed = speed;
+		}
+	}
+
