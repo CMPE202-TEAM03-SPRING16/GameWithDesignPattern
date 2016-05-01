c5cd62e78c18af2a13c896a31b0cbd6472e4fe2a
Gaurav Chodwadia <gauravchodwadia@gmail.com>
2016-04-30 23:44:57 -0700

    Script for destroy Done.

--- dev_game_project/Assets/Scripts/DestroyByContact.cs
+++ dev_game_project/Assets/Scripts/DestroyByContact.cs
@@ -4,11 +4,12 @@ using System.Collections;
 public class DestroyByContact : MonoBehaviour
 {
 
-	public GameObject explosion, playerExplosion;
+	public GameObject explosion, playerExplosion,pointLight;
 	public int scoreValue;
 	private Game_Controller gameController;
 	//private FalconController damage;
 	private int health;
+	private Light light;
 
 	void Start ()
 	{
@@ -20,41 +21,48 @@ public class DestroyByContact : MonoBehaviour
 		if (gameController == null) {
 			Debug.Log ("Cannot find 'GameController' script");
 		}
+		pointLight = GameObject.FindGameObjectWithTag ("PowerLight");
+		light = pointLight.GetComponent<Light>();
 	}
 
 
 	void OnTriggerEnter (Collider other)
 	{
 		
-		Debug.Log ("---->>>"+other.gameObject);
+
 		if (other.tag == "Boundary" || other.tag == "Enemy") {
 			return;
 		}
 		if (other.tag == "Health") {
 
-			Debug.Log ("Health ++");
-			gameController.AddHealth (20);
-			Destroy (other.gameObject);
-			Destroy (gameObject);
+
+			return;
+
 		}
 		if (other.tag == "Player") {
-			Debug.Log ("Player Hit");
-			//health = damage.TakeDamage (20);
-			Debug.Log ("Health " + health);
-
-			health = gameController.ReduceHealth (20);
-
-			Debug.Log (health == 0);
-			if (health == 0) {
-				Debug.Log ("Boom");
-				Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
-				Destroy (other.gameObject);
-				Destroy (gameObject);
-			} else {
-				Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+			Debug.Log ("Intensity "+light.intensity);
+			if (light.intensity == 1) {
+				light.intensity = 0;
 				return;
-			}
+			} else {
+				health = gameController.ReduceHealth (20);
+				Debug.Log ("<3 <3 <3"+health);
+				if (health == 0) {
+					Debug.Log ("Boom");
+					Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+					Destroy (other.gameObject);
+					Destroy (gameObject);
 
+					gameController.gameIsOver ();
+
+				} else {
+				
+					return;
+				}
+			}
+		}
+		if (other.tag == "Power") {
+			return;
 		}
 		if (explosion != null) {
 			
