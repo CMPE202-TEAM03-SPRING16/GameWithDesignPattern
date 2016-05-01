cccd400940a7b85d1366d1a616f4db30fc7700c2
chirag-orbittec <chirag.orbittec@gmail.com>
2016-04-30 22:53:44 -0700

    Lowerspeed code is done

---
+++ dev_game_project/Assets/Scripts/LowSpeedStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+	public class LowSpeedStrategy : ISpeedStrategy
+	{
+		public LowSpeedStrategy ()
+		{
+
+		}
+
+	public void SetSpeed(Mover character,int speed){
+			character.speed = speed;
+		}
+	}
+
